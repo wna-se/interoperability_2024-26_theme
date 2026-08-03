eb35e5b2cc64a8e95de1dea6bc585f8cfaa707d0
Wolmar Nyberg Åkerström <wolmar.n.akerstrom@uu.se>
2026-08-03 17:51:08 +0200

    Expand implicit relations

--- _plugins/generate_cff_site_variable.rb
+++ _plugins/generate_cff_site_variable.rb
@@ -21,8 +21,37 @@ Jekyll::Hooks.register :site, :post_read do |site|
     end
   end
 
+  cff_elements.select{ |id, e| e['type'] == 'process_phase' }.each do |phase_id, phase|
+    phase['outputs'] ||= []
+    phase['inputs'] ||= []
+
+    phase['outputs'].each do |output|
+      # output.update(cff_elements[output['id']].merge(output))
+      cff_elements[output['id']]['output_of'] = phase_id
+    end
+    phase['inputs'].each do |input|
+      # input.update(cff_elements[input['id']].merge(input))
+      cff_elements[input['id']]['input_of'] = phase_id
+    end
+  end
+
+  cff_elements.select{ |id, e| e['type'] == 'process_phase' }.each do |phase_id, phase|
+    phase['pre_phases'] = phase['inputs'].flat_map{ |e| cff_elements[e['id']]['output_of'] || [] }
+    phase['post_phases'] = phase['outputs'].flat_map{ |e| cff_elements[e['id']]['input_of'] || [] }
+  end
+
+  site.pages.each do |page|
+    if page.data['cff_properties'].is_a?(Hash)
+      site.regenerator.add_dependency(page.path, cff_elements)
+      page.data['cff'] = cff_elements
+    end
+  end
+  
+  #site.regenerator.add_dependency("_includes/cff-phase.md.liquid", cff_elements)
+  #site.regenerator.regenerate?("pages/framework/phase_2.md")
+
   site.config['cff'] = cff_elements
 
-  Jekyll.logger.info "CFF Site Variable Hook:", "Injected {{ site.cff.length }} CFF items from {{ site.pages.length }} pages."
+  Jekyll.logger.info "CFF Site Variable Hook:", "Injected #{ cff_elements.length } CFF items from #{ site.pages.length } pages."
 end
 
